74faf26c0fdc7595db6c5e05f7e74e560e93c0d5
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-01-23 21:20:11 +0200

    ...

--- Backend/controllers/sendConfirmationEmail.js
+++ Backend/controllers/sendConfirmationEmail.js
@@ -3,40 +3,37 @@ require("dotenv").config();
 
 const sendConfirmationEmail = async ({ from, to, subject, text }) => {
   // send email for password reset and email confirmation
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: process.env.EMAILPORT,
-      secure: process.env.SECURE,
-      auth: {
-        user: "finlunch.com@gmail.com",
-        pass: process.env.EMAILPASSWORD,
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    service: process.env.SERVICE,
+    port: process.env.EMAILPORT,
+    secure: process.env.SECURE,
+    auth: {
+      user: "finlunch.com@gmail.com",
+      pass: process.env.EMAILPASSWORD,
+    },
+    tls: { rejectUnauthorized: false },
+  });
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        from: from,
+        to: to,
+        subject: subject,
+        text: text,
       },
-      tls: { rejectUnauthorized: false },
-    });
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(
-        {
-          from: from,
-          to: to,
-          subject: subject,
-          text: text,
-        },
-        (err, info) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            resolve(info);
-          }
+      (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
         }
-      );
-      console.log("email sent");
-    });
-  } catch (err) {
-    console.log(err);
-  }
+      }
+    );
+  });
 };
 
 module.exports = sendConfirmationEmail;
