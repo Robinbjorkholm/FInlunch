7870f2480e0d7de53b841f83bc53bf15fb302e14
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-02-04 17:02:52 +0200

    .

--- client/src/components/Api/createNewFoodType.js
+++ client/src/components/Api/createNewFoodType.js
@@ -1,9 +1,20 @@
 import Axios from "axios";
 
+const headers = {
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+};
+
 export default function createNewFoodType(foodType) {
-  Axios.post(`${process.env.REACT_APP_APIENDPOINT}/foodTypes/createFoodType`, {
-    foodType: foodType,
-  }).catch(function (error) {
+  Axios.post(
+    `${process.env.REACT_APP_APIENDPOINT}/foodTypes/createFoodType`,
+    {
+      foodType: foodType,
+    },
+    { headers: headers }
+  ).catch(function (error) {
     console.log(error);
   });
 }
