a583215c00572e7614721ea4f708e1ddbb9e503b
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-02-04 17:01:44 +0200

    .

--- client/src/components/Api/deleteFoodType.js
+++ client/src/components/Api/deleteFoodType.js
@@ -5,7 +5,6 @@ export default function deleteFoodType(id) {
     .delete(
       `${process.env.REACT_APP_APIENDPOINT}/foodTypes/deleteFoodType/${id}`
     )
-
     .catch(function (error) {
       console.log(error);
     });
