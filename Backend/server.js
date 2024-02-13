eee02e2e4881bcad1428aead55bf961f552117ef
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-02-13 18:39:05 +0200

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+const port = process.env.PORT;
+require("./cors")(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/images", express.static("/images"));
 app.set("view engine", "ejs");
+app.use("/foods", food);
+app.use("/users", user);
+app.use("/comments", comment);
+app.use("/foodTypes", foodTypes);
+app.use("/likes", like);
 
-require("dotenv").config();
-require("./cors")(app);
-require("./routes/routes")(app);
-
-const port = process.env.PORT;
 app.listen(port || 3001, () => {
   console.log(`server running on port ${port}`);
 });
