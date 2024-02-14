35e6b63b54f7b314690778d14a02fe73f410ad68
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-02-14 19:29:35 +0200

    .

--- Backend/routes/foods.js
+++ Backend/routes/foods.js
@@ -6,10 +6,11 @@ const Food = require("../models/food");
 const foodController = require("../controllers/foodController");
 const authorization = require("../middleware/authorization");
 const admin = require("../middleware/admin");
+const uploadImage = require("../controllers/uploadImage");
 
 router.post(
   "/createFood",
-  [authorization, admin],
+  [authorization, admin, uploadImage],
   foodController.createFood
 );
 router.get("/getFoods", foodController.getFoods);
