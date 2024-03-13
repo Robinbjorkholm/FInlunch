862b62d6dd97cfa6ae0e56e20829ca18d631bcfc
Robin B <80909866+Robinbjorkholm@users.noreply.github.com>
2024-03-13 11:56:05 +0200

    .

--- Backend/routes/comments.js
+++ Backend/routes/comments.js
@@ -4,12 +4,14 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { body, validationResult } = require("express-validator");
 const commentController = require("../controllers/commentController");
 const authorization = require("../middleware/authorization");
+const admin = require("../middleware/admin");
 
 router.post("/createComment", authorization, commentController.createComment);
 router.get("/getComments", commentController.getComments);
 router.delete(
   "/deleteComment/:commentId",
   authorization,
+  admin,
   commentController.deleteComment
 );
 
